cfe288a07fba4e8dbeab2bcb3636659b4c770e4c
tim <tjhickey@brandeis.edu>
2018-02-13 14:56:35 -0500

    added route for fefu

--- theatergen/routes/index.js
+++ theatergen/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/fefu',function(req,res, next){
+	res.render('fefu',{title:'Fefu'});
+});
+
 module.exports = router;
